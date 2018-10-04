4f2b5efa0fd4da8e224f21ba62960a9000efd0db
kumad <>
2018-10-05 00:56:33 +0900

    超絶基本な仕組み

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('foo', function () {
+    return 'Foo!';
+});
+Route::get('foo/foo1', 'FooController@foo1');
+Route::get('foo/foo2', 'FooController@foo2');
+
