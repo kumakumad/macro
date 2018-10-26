a12d402ea6a47f4e098c4b13c741d38f7b9be705
kumad <ogoogo0210@gmail.com>
2018-10-26 23:10:51 +0900

    edit画面

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -65,9 +65,10 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Post $post)
     {
         //
+        return view('posts.edit', ['post' => $post]);
     }
 
     /**
@@ -77,9 +78,12 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Post $post)
     {
-        //
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->save();
+        return redirect('posts/' . $post->id);
     }
 
     /**
