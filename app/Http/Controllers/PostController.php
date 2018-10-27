2b59ca45a002ef6676c3c91a0b3451b6fa9051bd
kumad <ogoogo0210@gmail.com>
2018-10-27 20:15:31 +0900

    削除追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -86,14 +86,15 @@ class PostController extends Controller
         return redirect('posts/' . $post->id);
     }
 
-    /**
+        /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \App\Post  $post
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return redirect('posts');
     }
 }
