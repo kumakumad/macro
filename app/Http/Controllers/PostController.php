9367b57f8eebceeb00620ccffcc898c406816e25
kumad <ogoogo0210@gmail.com>
2018-11-04 23:42:21 +0900

    ログイン強制

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,6 +7,12 @@ use App\Post;
 
 class PostController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['index', 'show']);
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -44,6 +50,7 @@ class PostController extends Controller
         $post = new Post;
         $post->title = $request->title;
         $post->body = $request->body;
+        $post->user_id = $request->user()->id;
         $post->save();
         return redirect('posts/'.$post->id);
     }
